dba9a8716d3bf98d72e212d264c6c3b7e750666b
Yang, Minju (DI SW EDAGS PR TS EBSKR) <minju.yang@siemens.com>
2025-01-07 16:09:32 +0900

    add designer_doc

--- xpedition_manager/manager.py
+++ xpedition_manager/manager.py
@@ -1,5 +1,7 @@
 # xpedition_manager/manager.py
-from win32com.client import gencache, Dispatch
+from win32com.client import gencache, DispatchWithEvents, Dispatch
+import pythoncom
+import win32com #
 
 class XpeditionManager:
     def __init__(self):
@@ -65,6 +67,14 @@ class XpeditionManager:
         except Exception as e:
             print(f"Error initializing Design application: {e}")
 
+    def set_design_doc(self):
+        try:
+            self.design_doc = self.design_app.ActiveDocument
+            if self.design_app:
+                print("Xpedition Design Document initialized.")
+        except Exception as e:
+            print(f"Error initializing Design Document: {e}")
+
     def set_constraints_auto(self):
         try:
             self.constraints_auto = self._try_dispatch("ConstraintsAuto")
@@ -89,13 +99,13 @@ class XpeditionManager:
         seed = self.constraints_auto.Validate(0)
         self.constraints_auto.Validate(seed)
         return self.constraints_auto
-
+    
     def initialize_constraint_auto(self):
         self.set_release_env_server()
         self.set_constraints_auto()
         self.validate_cm()
-
-    # Actual initialize method
+    
+    # 실질적 환경설정 메서드
     def initialize_pcb(self):
         self.set_release_env_server()
         self.set_pcb_app()
@@ -106,13 +116,20 @@ class XpeditionManager:
     def initialize_design(self):
         self.set_release_env_server()
         self.set_design_app()
+        self.set_design_doc()
         self.initialize_constraint_auto()
 
     def initialize_both(self):
         self.set_release_env_server()
         self.set_design_app()
+        self.set_design_doc()
         self.set_pcb_app()
         self.set_pcb_doc()
         self.set_license_server()
         self.validate_pcb()
         self.initialize_constraint_auto()
+        self.pcb_doc = None
+        self.design_app = None
+        self.constraints_auto = None
+        self.command_listener = None
+        self.event_handler = None
